4502a392e5b725a5afba1a1c0d5d72cdb06890a6
Raymond Jette <rjette@mestek.com>
2022-04-01 16:12:53 -0400

    Remove unused helper function Test-IsGPOEmpty.

--- Private/Test-IsGPOEmpty.ps1
+++
@@ -1,7 +0,0 @@
-Function Test-IsGPOEmpty
-{
-    param (
-        [Microsoft.GroupPolicy.Gpo]$GPO
-    )
-    [bool](($GPO.user.dsversion -eq 0) -and ($GPO.computer.dsversion -eq 0))
-} # Test-IsGPOEmpty
